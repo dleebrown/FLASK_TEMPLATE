c4faf38ffd340d8d0ed91599702ddce88c012bae
donald <donald@admins-MacBook-Air-28.local>
2019-06-12 14:41:03 -0400

    fixed print statements

--- flaskexample/a_model.py
+++ flaskexample/a_model.py
@@ -1,6 +1,5 @@
 def ModelIt(fromUser  = 'Default', births = []):
  in_month = len(births)
- print('The number born is %i' % in_month)
  result = in_month
  if fromUser != 'Default':
    return result
